93bc21cea2eccc7d08f529e6add0c62dea54f685
Пользователь <rumyancev.01@mail.ru>
2026-08-20 12:01:14 +0300

    fix

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import functools
 import asyncio
+
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from loguru import logger
 from aiogram import Dispatcher, Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault
+from app.command_start import start_command
+from app.menu import router_menu
+from app.pay_button import router_pay
+from app.db.base import create_tables
+from config import SettingConfig
 
 dp = Dispatcher()
+dp.include_router(start_command)
+dp.include_router(router_menu)
+dp.include_router(router_pay)
 
 async def start_bot(bot: Bot):
     await set_commands(bot)
+    await create_tables()
     logger.success('Бот запустился!')
 
 async def stop_bot():
@@ -21,7 +33,7 @@ async def set_commands(bot: Bot):
 
 
 async def main():
-    bot = Bot()
+    bot = Bot(SettingConfig.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp.startup.register(functools.partial(start_bot, bot))
     dp.shutdown.register(stop_bot)
     await dp.start_polling(bot)
